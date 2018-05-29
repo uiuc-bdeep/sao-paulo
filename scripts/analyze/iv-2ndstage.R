b58d2310c4e9b3e5b94ce023bc823fcb39758a1e
Amanda Ang <35544965+aang4@users.noreply.github.com>
2018-05-29 16:02:48 -0500

    Update iv-2ndstage.R

--- scripts/analyze/iv-2ndstage.R
+++ scripts/analyze/iv-2ndstage.R
@@ -1,3 +1,16 @@
+# -------------------------------------------------------------------------------------- #
+#                                                                                        #
+# Regress trip travel time on predicted flood durations with spillovers                  #
+#                                                                                        #
+#                                                                                        #
+# Created by: Amanda Ang                                                                 #
+#             Big Data Environmental Economics and Policy Group                          #
+#                                                                                        #
+# -------------------------------------------------------------------------------------- #
+
+
+# Edited on 05/29/2018
+
 # clear workspace
 
 rm(list = ls())
@@ -5,7 +18,7 @@ gc()
 
 # set working directory
 
-setwd("//141.142.208.117/share/projects/Congestion/")
+setwd("/home/bdeep/share/projects/Congestion/")
 source("intermediate/floods/environment.R")
 
 #   loading packages
@@ -31,7 +44,7 @@ names(trips)
 
 # trip FE
 
-iv.1 <- felm(tr.time ~ fitted.blocks + fitted.floods | ID_ORDEM , data = trips)
+iv.1 <- felm(tr.time ~ fitted.blocks + fitted.floods | ID_ORDEM | 0 | ID_ORDEM, data = trips)
 
 iv1.coef <- as.data.frame(summary(iv.1)$coefficients)
 iv1.coef$model <- "iv.1"
@@ -40,7 +53,7 @@ rm(iv.1)
 
 # trip + month FE
 
-iv.2 <- felm(tr.time ~ fitted.blocks + fitted.floods | ID_ORDEM + month, data = trips)
+iv.2 <- felm(tr.time ~ fitted.blocks + fitted.floods | ID_ORDEM + month | 0 | ID_ORDEM, data = trips)
 
 iv2.coef <- as.data.frame(summary(iv.2)$coefficients)
 iv2.coef$model <- "iv.2"
@@ -49,7 +62,7 @@ rm(iv.2)
 
 # trip + month + day of week FE
 
-iv.3 <- felm(tr.time ~ fitted.blocks + fitted.floods | ID_ORDEM + month + wd, data = trips)
+iv.3 <- felm(tr.time ~ fitted.blocks + fitted.floods | ID_ORDEM + month + wd | 0 | ID_ORDEM, data = trips)
 
 iv3.coef <- as.data.frame(summary(iv.3)$coefficients)
 iv3.coef$model <- "iv.3"
@@ -58,7 +71,7 @@ rm(iv.3)
 
 # trip + month + day of week + time of day FE
 
-iv.4 <- felm(tr.time ~ fitted.blocks + fitted.floods | ID_ORDEM + month + wd + hour.f , data = trips)
+iv.4 <- felm(tr.time ~ fitted.blocks + fitted.floods | ID_ORDEM + month + wd + hour.f | 0 | ID_ORDEM, data = trips)
 
 iv4.coef <- as.data.frame(summary(iv.4)$coefficients)
 iv4.coef$model <- "iv.4"
@@ -79,9 +92,10 @@ stargazer(iv.1, iv.2, iv.3, iv.4,
           title = "IV Second Stage",
           covariate.labels = c("Blocks",
                                "Floods"),
-          add.lines = list(c("Trip FE", "Yes", "Yes", "Yes", "Yes"),
-                           c("Month FE", "No", "Yes", "Yes", "Yes"),
-                           c("Day of Week FE", "No", "No", "Yes", "Yes"),
-                           c("Hour FE", "No", "No", "No", "Yes")),
-          dep.var.labels = "ln(Trip Duration)",
+          add.lines = list(c("Trip FE", "Y", "Y", "Y", "Y"),
+                           c("Month FE", "N", "Y", "Y", "Y"),
+                           c("Day of Week FE", "N", "N", "Y", "Y"),
+                           c("Hour FE", "N", "N", "N", "Y")),
+          notes = "Standard errors are clustered at the trip level.",
+          dep.var.labels = "Trip Duration (minutes)",
           out = paste0(out, "iv-secondstage.tex"))
