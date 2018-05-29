b29a246157cdd3c6a06a9a282be8f697d21cd081
Amanda Ang <35544965+aang4@users.noreply.github.com>
2018-05-29 12:01:16 -0500

    Update iv-1ststage.R

--- scripts/analyze/iv-1ststage.R
+++ scripts/analyze/iv-1ststage.R
@@ -1,3 +1,14 @@
+# ---------------------------------------------------------------------------------------- #
+#                                                                                          #
+# Regress flood duration on blocks and floods                                              #
+#                                                                                          #
+# Created by: Amanda Ang                                                                   #
+#             Big Data Environmental Economics and Policy Group                            #
+#                                                                                          #
+# ---------------------------------------------------------------------------------------- #
+
+# Edited on 05/29/2018
+
 # clear workspace
 
 rm(list = ls())
@@ -5,7 +16,7 @@ gc()
 
 # set working directory
 
-setwd("//141.142.208.117/share/projects/Congestion/")
+setwd("/home/bdeep/share/projects/Congestion/")
 source("intermediate/floods/environment.R")
 
 #   loading packages
@@ -15,12 +26,11 @@ lapply(packages, pkgTest)
 
 #   input
 
-# trips.path <- "intermediate/floods/date-merge.rds"
 trips.path <- "intermediate/floods/floods-model.rds"
 
 #   output
 
-out <- "documents/working paper/floods/tables/"
+out.path <- "views/floods/"
 
 #   read files
 
@@ -39,30 +49,16 @@ trips$fduration1 <- as.numeric(as.character(trips$fduration1))
 
 # blocks
 
-iv.1 <- felm(duration1 ~ blocks:rain.bins1 + blocks:rain.bins2 + blocks:rain.bins3 | ID_ORDEM + month + wd + hour.f, data = trips)
-
-trips$fitted.blocks <- as.numeric(fitted(iv.1))
-iv1.coef <- as.data.frame(summary(iv.1)$coefficients)
-iv1.coef$model <- "iv.1"
-
-rm(iv.1)
+iv.1 <- felm(duration.mean ~ blocks:rain.bins1 + blocks:rain.bins2 + blocks:rain.bins3 | month + wd + hour.f | 0 | ID_ORDEM, data = trips)
 
 # floods
 
-iv.2 <- felm(fduration1 ~ floods:rain.bins1 + floods:rain.bins2 + floods:rain.bins3 | month + wd + hour.f, data = trips)
+iv.2 <- felm(fduration.mean ~ floods:rain.bins1 + floods:rain.bins2 + floods:rain.bins3 | month + wd + hour.f | 0 | ID_ORDEM, data = trips)
 
-trips$fitted.floods <- as.numeric(fitted(iv.2))
-iv2.coef <- as.data.frame(summary(iv.2)$coefficients)
-iv2.coef$model <- "iv.2"
-
-# generate table -------------------------------------------------------------------------------
+# output ----------------------------------------------------------------------------------------
 
 stargazer(iv.1, iv.2, 
           align = TRUE,
           type = "latex",
           title = "IV 1st Stage",
-          out = paste0(out, "iv-1ststage.tex"))
-
-# save output ---------------------------------------------------------------------------------------
-
-saveRDS(trips, trips.path)
+          out = paste0(out.path, "iv-1ststage.tex"))
