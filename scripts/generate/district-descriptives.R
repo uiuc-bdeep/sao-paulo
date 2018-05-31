80810614f7e934fe72d3fa03108fde24ca689792
Amanda Ang <35544965+aang4@users.noreply.github.com>
2018-05-31 12:49:32 -0500

    Update district-descriptives.R

--- scripts/generate/district-descriptives.R
+++ scripts/generate/district-descriptives.R
@@ -11,10 +11,27 @@ rm(list = ls())
 # inputs 
 
 floods.path <- "intermediate/floods/floods.rds"
+survey.path <- "intermediate/floods/survey-sub-2012.rds"
 trips.path <- "intermediate/floods/floods-model.rds"
 
 # read data 
 
 floods <- readRDS(floods.path)
+survey <- readRDS(survey.path)
+trips <- readRDS(trips.path)
+
+# merge districts from 2012 survey with crawled trips data
+
+survey <- as.data.table(survey)
+survey <- survey[,c("ID_ORDEM", "SUB")]
+
+trips <- as.data.table(trips)
+trips <- merge(trips, survey, by = "ID_ORDEM", all.x = TRUE)
+
+# generate number of floods per district
+
+floods <- group_by(floods, SUB, SITUACAO)
+floods1 <- summarise(floods, count = n())
+
 
 
