4a98bbd70f5b783ba9d3e0bcbfc43240f36d9cc5
Amanda Ang <35544965+aang4@users.noreply.github.com>
2018-06-01 17:15:41 -0500

    Add rain bins

--- scripts/analyze/iv-2ndstage.R
+++ scripts/analyze/iv-2ndstage.R
@@ -43,39 +43,35 @@ names(trips)
 
 # trip FE
 
-iv.1 <- felm(tr.time ~ fitted.blocks + fitted.floods | ID_ORDEM | 0 | ID_ORDEM, data = trips)
+iv.1 <- felm(tr.time ~ fitted.blocks + fitted.floods + rain.bins1 + rain.bins2 + rain.bins3
+                       | ID_ORDEM | 0 | ID_ORDEM, data = trips)
 
 iv1.coef <- as.data.frame(summary(iv.1)$coefficients)
 iv1.coef$model <- "iv.1"
 
-rm(iv.1)
-
 # trip + month FE
 
-iv.2 <- felm(tr.time ~ fitted.blocks + fitted.floods | ID_ORDEM + month | 0 | ID_ORDEM, data = trips)
+iv.2 <- felm(tr.time ~ fitted.blocks + fitted.floods + rain.bins1 + rain.bins2 + rain.bins3
+                       | ID_ORDEM + month | 0 | ID_ORDEM, data = trips)
 
 iv2.coef <- as.data.frame(summary(iv.2)$coefficients)
 iv2.coef$model <- "iv.2"
 
-rm(iv.2)
-
 # trip + month + day of week FE
 
-iv.3 <- felm(tr.time ~ fitted.blocks + fitted.floods | ID_ORDEM + month + wd | 0 | ID_ORDEM, data = trips)
+iv.3 <- felm(tr.time ~ fitted.blocks + fitted.floods + rain.bins1 + rain.bins2 + rain.bins3
+                       | ID_ORDEM + month + wd | 0 | ID_ORDEM, data = trips)
 
 iv3.coef <- as.data.frame(summary(iv.3)$coefficients)
 iv3.coef$model <- "iv.3"
 
-rm(iv.3)
-
 # trip + month + day of week + time of day FE
 
-iv.4 <- felm(tr.time ~ fitted.blocks + fitted.floods | ID_ORDEM + month + wd + hour.f | 0 | ID_ORDEM, data = trips)
+iv.4 <- felm(tr.time ~ fitted.blocks + fitted.floods + rain.bins1 + rain.bins2 + rain.bins3
+                       | ID_ORDEM + month + wd + hour.f | 0 | ID_ORDEM, data = trips)
 
 iv4.coef <- as.data.frame(summary(iv.4)$coefficients)
 iv4.coef$model <- "iv.4"
-
-rm(iv.4)
 
 #   output -------------------------------------------------------------------------------------
 
@@ -86,12 +82,14 @@ saveRDS(coef, coef.path)
 
 # LaTeX table
 stargazer(iv.1, iv.2, iv.3, iv.4, 
-           align = TRUE,
            type = "latex",
            df = FALSE,
            title = "IV Second Stage",
-           covariate.labels = c("pr.Blocks",
-                                "pr.Floods"),
+           covariate.labels = c("pr.Blocks Duration",
+                                "pr.Floods Duration",
+                                 "Light Rain",
+                                 "Moderate Rain",
+                                 "Heavy Rain"),
            add.lines = list(c("Trip FE", "Y", "Y", "Y", "Y"),
                             c("Month FE", "N", "Y", "Y", "Y"),
                             c("Day of Week FE", "N", "N", "Y", "Y"),
