0c078018cd2e8bb5c9cb53777ae96d64e8711660
Amanda Ang <35544965+aang4@users.noreply.github.com>
2018-06-04 15:12:57 -0500

    add rain bins

--- scripts/spillovers/iv-2ndstage.R
+++ scripts/spillovers/iv-2ndstage.R
@@ -44,28 +44,32 @@ names(trips)
 
 # trip FE
 
-iv.1 <- felm(tr.time ~ fitted.blocks + fitted.floods + fitted.spill | ID_ORDEM | 0 | ID_ORDEM, data = trips)
+iv.1 <- felm(tr.time ~ fitted.blocks + fitted.floods + fitted.spill +
+                       rain.bins1 + rain.bins2 + rain.bins3 | ID_ORDEM | 0 | ID_ORDEM, data = trips)
 
 iv1.coef <- as.data.frame(summary(iv.1)$coefficients)
 iv1.coef$model <- "iv.1"
 
 # trip + month FE
 
-iv.2 <- felm(tr.time ~ fitted.blocks + fitted.floods + fitted.spill | ID_ORDEM + month | 0 | ID_ORDEM, data = trips)
+iv.2 <- felm(tr.time ~ fitted.blocks + fitted.floods + fitted.spill +
+                       rain.bins1 + rain.bins2 + rain.bins3 | ID_ORDEM + month | 0 | ID_ORDEM, data = trips)
 
 iv2.coef <- as.data.frame(summary(iv.2)$coefficients)
 iv2.coef$model <- "iv.2"
 
 # trip + month + day of week FE
 
-iv.3 <- felm(tr.time ~ fitted.blocks + fitted.floods + fitted.spill | ID_ORDEM + month + wd | 0 | ID_ORDEM, data = trips)
+iv.3 <- felm(tr.time ~ fitted.blocks + fitted.floods + fitted.spill +
+                       rain.bins1 + rain.bins2 + rain.bins3 | ID_ORDEM + month + wd | 0 | ID_ORDEM, data = trips)
 
 iv3.coef <- as.data.frame(summary(iv.3)$coefficients)
 iv3.coef$model <- "iv.3"
 
 # trip + month + day of week + time of day FE
 
-iv.4 <- felm(tr.time ~ fitted.blocks + fitted.floods + fitted.spill | ID_ORDEM + month + wd + hour.f | 0 | ID_ORDEM, data = trips)
+iv.4 <- felm(tr.time ~ fitted.blocks + fitted.floods + fitted.spill +
+                       rain.bins1 + rain.bins2 + rain.bins3| ID_ORDEM + month + wd + hour.f | 0 | ID_ORDEM, data = trips)
 
 iv4.coef <- as.data.frame(summary(iv.4)$coefficients)
 iv4.coef$model <- "iv.4"
@@ -81,14 +85,17 @@ saveRDS(coef, coef.path)
 stargazer(iv.1, iv.2, iv.3, iv.4, 
           type = "latex",
           df = FALSE,
-          title = "IV Second Stage",
-          covariate.labels = c("pr.Blocks",
-                               "pr.Floods",
-                               "pr.Spillovers"),
+          title = "IV Second Stage with Spillovers",
           add.lines = list(c("Trip FE", "Y", "Y", "Y", "Y"),
                            c("Month FE", "N", "Y", "Y", "Y"),
                            c("Day of Week FE", "N", "N", "Y", "Y"),
                            c("Hour FE", "N", "N", "N", "Y")),
           notes = "Standard errors are clustered at the trip level.",
           dep.var.labels = "Trip Duration (minutes)",
+          covariate.labels = c("pr.Blocks Duration",
+                               "pr.Floods Duration", 
+                               "pr.Spillovers Duration",
+                               "Light Rain", 
+                               "Moderate Rain",
+                               "Heavy Rain"),
           out = paste0(out.path, "iv-secondstage.tex"))
