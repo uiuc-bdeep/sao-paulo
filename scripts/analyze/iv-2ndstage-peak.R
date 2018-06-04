bdfd4e66a68e64e9542d205420eace3bcd188ae2
Amanda Ang <35544965+aang4@users.noreply.github.com>
2018-06-04 10:03:14 -0500

    Add rain bins

--- scripts/analyze/iv-2ndstage-peak.R
+++ scripts/analyze/iv-2ndstage-peak.R
@@ -31,7 +31,7 @@ trips.path <- "intermediate/floods/floods-model.rds"
 #   output
 
 coef2.path <- "intermediate/floods/iv2-coef (peak).rds"
-out <- "documents/working paper/floods/tables/"
+out.path <- "views/floods/"
 
 # read files -----------------------------------------------------------------------------------
 
@@ -39,58 +39,32 @@ trips <- readRDS(trips.path)
 
 # second stage with peak hour indicators -------------------------------------------------------
 
-# trip FE 
-# cluster standard errors at trip level
-
-iv.1 <- felm(tr.time ~ fitted.blocks:early.peak + fitted.floods:early.peak + 
-                       fitted.blocks:late.peak + fitted.floods:late.peak + 
-                       fitted.blocks:off.peak + fitted.floods:off.peak | ID_ORDEM | 0 | ID_ORDEM, data = trips)
-
-iv1.coef <- as.data.frame(summary(iv.1)$coefficients)
-iv1.coef$model <- "iv.1"
-
-# trip + month FE
-# cluster standard errors at trip level
-
-iv.2 <- felm(tr.time ~ fitted.blocks:early.peak + fitted.floods:early.peak + 
-                       fitted.blocks:late.peak + fitted.floods:late.peak + 
-                       fitted.blocks:off.peak + fitted.floods:off.peak | ID_ORDEM + month | 0 | ID_ORDEM, data = trips)
-
-iv2.coef <- as.data.frame(summary(iv.2)$coefficients)
-iv2.coef$model <- "iv.2"
-
-# trip + month + day of week FE
-# cluster standard errors at trip level
-
-iv.3 <- felm(tr.time ~ fitted.blocks:early.peak + fitted.floods:early.peak + 
-                       fitted.blocks:late.peak + fitted.floods:late.peak + 
-                       fitted.blocks:off.peak + fitted.floods:off.peak  | ID_ORDEM + month + wd | 0 | ID_ORDEM, data = trips)
-
-iv3.coef <- as.data.frame(summary(iv.3)$coefficients)
-iv3.coef$model <- "iv.3"
-
 # trip + month + day of week + time of day FE
 
-iv.4 <- felm(tr.time ~ fitted.blocks:early.peak + fitted.floods:early.peak +
-                       fitted.blocks:late.peak + fitted.floods:late.peak +
-                       fitted.blocks:off.peak + fitted.floods:off.peak  | ID_ORDEM + month + wd + hour.f | 0 | ID_ORDEM, data = trips)
+iv <- felm(tr.time ~ fitted.blocks:early.peak + fitted.floods:early.peak +
+                     fitted.blocks:late.peak + fitted.floods:late.peak +
+                     fitted.blocks:off.peak + fitted.floods:off.peak + 
+                     rain.bins1 + rain.bins2 + rain.bins3 | ID_ORDEM + month + wd + hour.f | 0 | ID_ORDEM, data = trips)
 
-iv4.coef <- as.data.frame(summary(iv.4)$coefficients)
-iv4.coef$model <- "iv.4"
+iv.coef <- as.data.frame(summary(iv)$coefficients)
 
 # output ---------------------------------------------------------------------------------------
 
 # save coefficients
 
-coef2 <- rbind(iv1.coef, iv2.coef, iv3.coef, iv4.coef)
-saveRDS(coef2, coef2.path)
+saveRDS(iv.coef, coef2.path)
 
 # generate LaTeX table 
 
-stargazer(iv.1, iv.2, iv.3, iv.4, 
-          align = TRUE,
+stargazer(iv,
           type = "latex",
+          dep.var.labels = c("Trip Duration"),
           df = FALSE,
-          out = paste0(out, "iv (peak).tex"))
+          add.lines = list(c("Trip FE", "Y"),
+                           c("Month FE", "Y"),
+                           c("Day of Week FE", "Y"),
+                           c("Hour FE", "Y")),
+          notes = "Standard errors are clustered at the trip level.",
+          out = paste0(out.path, "iv (peak).tex"))
 
 
