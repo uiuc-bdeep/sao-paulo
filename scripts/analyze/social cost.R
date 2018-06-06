29f12ccc88fb97841120aff6426625fe430d314b
Amanda Ang <35544965+aang4@users.noreply.github.com>
2018-06-05 23:15:13 -0500

    Add peak hours and traffic directions

--- scripts/analyze/social cost.R
+++ scripts/analyze/social cost.R
@@ -24,6 +24,9 @@ floods.path <- "intermediate/floods/floods.rds"
 coef.path <- "intermediate/floods/iv1-coef.rds"
 coef2.path <- "intermediate/floods/iv2-coef.rds"
 
+peak.path <- "intermediate/floods/iv2-coef (peak).rds" 
+dir.path <- "intermediate/floods/iv2-coef(traffic).rds"
+
 # ----------------------------------------------------------------------------------------------
 # generating predicted values for blocks, floods and spillovers
 
@@ -43,6 +46,7 @@ floods.rain <- coef$Estimate[[8]]
 coef2 <- readRDS(coef2.path)
 
 # assign names to each coefficient
+
 coef2 <- coef2[which(coef2$model == "iv.4"),] # taking model that includes the most FEs
 
 pr.blocks <- coef2$Estimate[[1]]
@@ -128,4 +132,130 @@ HH$CS2 <- 0.5 * HH$hourly.income.pwaa * (Floods / 60)
 CSD <- sum(HH$CS2 * HH$FE_PESS, na.rm = TRUE)
 CSD
 (CSD / SP.GDP) * 100
+
+# ----------------------------------------------------------------------------------------------
+
+# Social Cost with Peak Hours
+
+peak.coef <- readRDS(peak.path)
+
+blocks.early <- peak.coef$Estimate[[4]] * blocks.rain * block.days
+blocks.late <- peak.coef$Estimate[[6]] * blocks.rain * block.days
+blocks.off <- peak.coef$Estimate[[8]] * blocks.rain * block.days
+
+floods.early <- peak.coef$Estimate[[5]] * floods.rain * flood.days
+floods.late <- peak.coef$Estimate[[7]] * floods.rain * flood.days
+floods.off <- peak.coef$Estimate[[9]] * floods.rain * flood.days
+
+# Morning Peak
+HH$MP <- as.numeric(ifelse(HH$dep.hour >= 7 & 
+                             HH$dep.hour < 11, 1, 0))
+summary(HH$MP)
+
+# Evening Peak
+HH$EP <- as.numeric(ifelse(HH$hour >= 17 &
+                             HH$hour < 21, 1, 0))
+summary(HH$EP)
+
+# Off-Peak
+HH$OP <- as.numeric(ifelse(HH$MP == 0 & HH$EP == 0, 1, 0))
+summary(HH$OP)
+
+# ----------------------------------------------------------------------------------------------
+
+# Blocks
+HH$CS1 <- as.numeric(ifelse(HH$MP == 1, 0.5 * HH$hourly.income.pwaa * (blocks.early / 60),
+                            ifelse(HH$EP == 1, 0.5 * HH$hourly.income.pwaa * (blocks.late / 60),
+                                   ifelse(HH$OP == 1, 0.5 * HH$hourly.income.pwaa * (blocks.off / 60), NA))))
+
+CSD <- sum(HH$CS1 * HH$FE_PESS, na.rm = TRUE)
+CSD
+
+# Floods
+
+HH$CS2 <- as.numeric(ifelse(HH$MP == 1, 0.5 * HH$hourly.income.pwaa * (floods.early / 60),
+                            ifelse(HH$EP == 1, 0.5 * HH$hourly.income.pwaa * (floods.late / 60),
+                                   ifelse(HH$OP == 1, 0.5 * HH$hourly.income.pwaa * (floods.off / 60), NA))))
+
+CSD <- sum(HH$CS2 * HH$FE_PESS, na.rm = TRUE)
+CSD
+
+# ----------------------------------------------------------------------------------------------
+
+# Social Cost with Traffic Directions 
+
+dir.coef <- readRDS(dir.path)
+
+blocks.with <- dir.coef$Estimate[[1]] * blocks.rain * block.days
+blocks.against <- dir.coef$Estimate[[3]] * blocks.rain * block.days
+blocks.off <- dir.coef$Estimate[[5]] * blocks.rain * block.days
+
+floods.with <- dir.coef$Estimate[[2]] * floods.rain * flood.days
+floods.against <- dir.coef$Estimate[[4]] * floods.rain * flood.days
+floods.off <- dir.coef$Estimate[[6]] * floods.rain * flood.days
+
+# create indicator for trips that end in the downtown area -------------------------------------
+# downtown defined as 2012 Survey Traffic Zones 1, 14, 15, 16 and 23
+
+HH$traffic_d <- as.numeric(ifelse(HH$ZONA_D == 1  |
+                                    HH$ZONA_D == 14 |
+                                    HH$ZONA_D == 15 |
+                                    HH$ZONA_D == 16 |
+                                    HH$ZONA_D == 23 , 1, 0))
+summary(HH$traffic_d)
+
+# create indicator for trips that begin in downtown area --------------------------------------
+
+HH$traffic_o <- as.numeric(ifelse(HH$ZONA_O == 1  |
+                                    HH$ZONA_O == 14 |
+                                    HH$ZONA_O == 15 |
+                                    HH$ZONA_O == 16 |
+                                    HH$ZONA_O == 23 , 1, 0))
+summary(HH$traffic_o)
+
+# create indicator for peak hours --------------------------------------------------------------
+# Morning Peak
+HH$MP <- as.numeric(ifelse(HH$dep.hour >= 7 & 
+                             HH$dep.hour < 11, 1, 0))
+summary(HH$MP)
+
+# Evening Peak
+HH$EP <- as.numeric(ifelse(HH$hour >= 17 &
+                             HH$hour < 21, 1, 0))
+summary(HH$EP)
+
+# Off-Peak
+HH$OP <- as.numeric(ifelse(HH$MP == 0 & HH$EP == 0, 1, 0))
+summary(HH$OP)
+
+# create indicators for traffic direction -------------------------------------------------------
+
+HH$with.traffic <- as.numeric(ifelse((HH$traffic_d == 1 & HH$MP == 1) |
+                                     (HH$traffic_o == 1 & HH$EP == 1), 1, 0))
+summary(HH$with.traffic)
+
+HH$against.traffic <- as.numeric(ifelse(((HH$traffic_o == 1 & HH$MP == 1) |
+                                        (HH$traffic_d == 1 & HH$EP == 1)) 
+                                        & HH$OP == 0, 1, 0))
+summary(HH$against.traffic)
+
+# Value of Time = 0.5 hourly income per working age adult 
+
+# Blocks
+HH$CS1 <- as.numeric(ifelse(HH$with.traffic == 1, 0.5 * HH$hourly.income.pwaa * (blocks.with / 60),
+                            ifelse(HH$against.traffic == 1, 0.5 * HH$hourly.income.pwaa * (blocks.against / 60),
+                                   ifelse(HH$OP == 1, 0.5 * HH$hourly.income.pwaa * (blocks.off / 60), NA))))
+
+CSD <- sum(HH$CS1 * HH$FE_PESS, na.rm = TRUE)
+CSD
+
+# Floods
+
+HH$CS2 <- as.numeric(ifelse(HH$with.traffic == 1, 0.5 * HH$hourly.income.pwaa * (floods.with / 60),
+                            ifelse(HH$against.traffic == 1, 0.5 * HH$hourly.income.pwaa * (floods.against / 60),
+                                   ifelse(HH$OP == 1, 0.5 * HH$hourly.income.pwaa * (floods.off / 60), NA))))
+
+CSD <- sum(HH$CS2 * HH$FE_PESS, na.rm = TRUE)
+CSD
+
 
