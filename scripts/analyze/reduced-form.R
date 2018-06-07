fee80a06123bfe40b30ac5950319e330b76c5b34
Amanda Ang <35544965+aang4@users.noreply.github.com>
2018-06-07 15:37:24 -0500

    add stargazer for m5

--- scripts/analyze/reduced-form.R
+++ scripts/analyze/reduced-form.R
@@ -58,19 +58,12 @@ m3 <- felm(tr.time ~ blocks:duration.mean + floods:fduration.mean
 m4 <- felm(tr.time ~ blocks:duration.mean + floods:fduration.mean 
                      + rain.bins1 + rain.bins2 + rain.bins3 | ID_ORDEM + month + wd + hour.f | 0 | ID_ORDEM, data = trips)
 
-
-# reduced form model with peak hour interactions -----------------------------------------------
-
-m5 <- felm(tr.time ~ blocks:duration.mean:early.peak + floods:fduration.mean:early.peak + 
-                     blocks:duration.mean:late.peak + floods:fduration.mean:late.peak + 
-                     blocks:duration.mean:not.peak + floods:fduration.mean:not.peak 
-                     + rain.bins1 + rain.bins2 + rain.bins3 | ID_ORDEM + month + wd + hour.f | 0 | ID_ORDEM, data = trips)
-
 # output ----------------------------------------------------------------------------------------
 
 stargazer(m1, m2, m3, m4,
           type = "latex",
           df = FALSE,
+          title = "Reduced Form Model",
           dep.var.labels = c("Trip Duration"),
           covariate.labels = c("Blocks Duration",
                                "Floods Duration",
@@ -81,7 +74,26 @@ stargazer(m1, m2, m3, m4,
           add.lines = list(c("Trip FE", "Y", "Y", "Y", "Y"),
                            c("Month FE", "N", "Y", "Y", "Y"),
                            c("Day of Week FE", "N", "N", "Y", "Y"),
-                           c("Hour FE", "N", "N", "N", "N")),
+                           c("Hour FE", "N", "N", "N", "Y")),
                    out = paste0(out.path, "reduced-form.tex"))
 
+# reduced form model with peak hour interactions -----------------------------------------------
+
+m5 <- felm(tr.time ~ blocks:duration.mean:early.peak + floods:fduration.mean:early.peak + 
+                     blocks:duration.mean:late.peak + floods:fduration.mean:late.peak + 
+                     blocks:duration.mean:not.peak + floods:fduration.mean:not.peak 
+                     + rain.bins1 + rain.bins2 + rain.bins3 | ID_ORDEM + month + wd + hour.f | 0 | ID_ORDEM, data = trips)
+
+
+stargazer(m5,
+          type = "latex",
+          df = FALSE,
+          title = "Reduced Form Model with Peak Hour Indicators",
+          dep.var.labels = c("Trip Duration"),
+          notes = "Standard errors clustered at trip level.",
+          add.lines = list(c("Trip FE", "Y"),
+                           c("Month FE", "Y"),
+                           c("Day of Week FE", "Y"),
+                           c("Hour FE", "Y")),
+          out = paste0(out.path, "peak-hour.tex"))
 
