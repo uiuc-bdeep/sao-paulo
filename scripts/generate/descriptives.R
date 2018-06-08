b847481529424760794990c5e7a7deb813add1fe
Amanda Ang <35544965+aang4@users.noreply.github.com>
2018-06-08 11:05:20 -0500

    Remove floods

--- scripts/generate/descriptives.R
+++ scripts/generate/descriptives.R
@@ -27,133 +27,17 @@ lapply(packages, pkgTest)
 
 # inputs
 
-floods.path <- "intermediate/floods/floods.rds"
-HH.path <- "intermediate/floods/floods-intersect.rds"
+HH.path <- "analyze/HH.rds"
 trips.path <- "intermediate/floods/floods-model.rds"
   
 # output
 
 out.path <- "views/floods/"
   
-# descriptive statistics for floods dataset ----------------------------------------------------
-
-# read files
-
-floods <- readRDS(floods.path)
-names(floods)
-
-# whole dataset
-var <- as.data.frame(floods$DURACAO)
-sum <- as.data.frame(capture.output(summary(var)))
-
-# format data frame
-# remove first row
-sum <- as.data.frame(sum[-1,])
-
-# format names
-names(sum)[which(names(sum) == "sum[-1, ]")] <- "Flood Events"
-
-# separate strings from numerical values
-sum <- separate(sum, `Flood Events`, into = c("stat", "Flood Events"), sep = ":", extra = "merge")
-
-# convert to numeric
-sum$`Flood Events` <- as.numeric(as.character(sum$`Flood Events`))
-
-# transpose
-rownames(sum) <- sum$stat
-sum$stat <- NULL
-
-sum <- as.data.frame(t(sum))
-
-# add variable for number of observations
-sum$Count <- as.numeric(nrow(nofloods))
-
-# add variable for share of total
-sum$Share <- nrow(floods) / nrow(floods)
-
-# output
-out <- as.data.frame(sum)
-
-# blocks ---------------------------------------------------------------------------------------
-
-blocks <- subset(floods, SITUACAO == "intransitavel")
-var <- as.data.frame(blocks$DURACAO)
-
-# save output as data frame
-blocks.sum <- as.data.frame(capture.output(summary(var)))
-
-# format data frame
-# remove first row
-blocks.sum <- as.data.frame(blocks.sum[-1,])
-
-# format names
-names(blocks.sum)[which(names(blocks.sum) == "blocks.sum[-1, ]")] <- "Blocks"
-
-# separate strings from numerical values
-blocks.sum <- separate(blocks.sum, Blocks, into = c("stat", "Blocks"), sep = ":", extra = "merge")
-
-# convert to numeric
-blocks.sum$Blocks <- as.numeric(as.character(blocks.sum$Blocks))
-
-# transpose
-rownames(blocks.sum) <- blocks.sum$stat
-blocks.sum$stat <- NULL
-
-blocks.sum <- as.data.frame(t(blocks.sum))
-
-# add variable for number of observations
-blocks.sum$Count <- as.numeric(nrow(blocks))
-
-# add variable for share of total
-blocks.sum$Share <- nrow(blocks) / nrow(floods)
-
-# floods ---------------------------------------------------------------------------------------
-
-floods <- subset(floods, SITUACAO == "transitavel")
-var <- as.data.frame(floods$DURACAO)
-
-# save output as data frame
-floods.sum <- as.data.frame(capture.output(summary(var)))
-
-# format data frame
-# remove first row
-floods.sum <- as.data.frame(floods.sum[-1,])
-
-# format names
-names(floods.sum)[which(names(floods.sum) == "floods.sum[-1, ]")] <- "Floods"
-
-# separate strings from numerical values
-floods.sum <- separate(floods.sum, floods, into = c("stat", "Floods"), sep = ":", extra = "merge")
-
-# convert to numeric
-floods.sum$Floods <- as.numeric(as.character(floods.sum$Floods))
-
-# transpose
-rownames(floods.sum) <- floods.sum$stat
-floods.sum$stat <- NULL
-
-floods.sum <- as.data.frame(t(floods.sum))
-
-# add variable for number of observations
-floods.sum$Count <- as.numeric(nrow(floods))
-
-# add variable for share of total
-floods.sum$Share <- nrow(floods) / 740
-
-# append to output
-
-out <- rbind(blocks.sum, floods.sum)
-
-rm(blocks, floods)
-
 # survey trips ---------------------------------------------------------------------------------
 
 HH <- readRDS(HH.path)
 names(HH)
-
-# subset to trips taken by car
-
-HH <- subset(HH, TIPOVG == 2)
 
 # keeping only relevant variables
 
@@ -184,14 +68,8 @@ sum$stat <- NULL
 
 sum <- as.data.frame(t(sum))
 
-# add variable for number of observations
-sum$Count <- as.numeric(nrow(HH))
-
-# add variable for share of total
-sum$Share <- nrow(HH) / nrow(HH)
-
 # append to output
-out <- rbind(out, sum)
+out <- sum 
 
 # blocks ---------------------------------------------------------------------------------------
 
@@ -216,12 +94,6 @@ rownames(blocks.sum) <- blocks.sum$stat
 blocks.sum$stat <- NULL
 
 blocks.sum <- as.data.frame(t(blocks.sum))
-
-# add variable for number of observations
-blocks.sum$Count <- as.numeric(nrow(blocks))
-
-# add variable for share of total
-blocks.sum$Share <- nrow(blocks) / 15483
 
 # append to output
 
@@ -303,6 +175,10 @@ nofloods.sum$Share <- nrow(nofloods) / 15483
 # append to output
 
 out <- rbind(out, nofloods.sum)
+
+# income bins ----------------------------------------------------------------------------------
+
+
 
 # crawled trips --------------------------------------------------------------------------------
 
