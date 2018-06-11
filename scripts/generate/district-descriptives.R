0a04928c76b3e2a8e0ff8971cff6bf5eae918a02
Amanda Ang <35544965+aang4@users.noreply.github.com>
2018-06-11 15:05:46 -0500

    Change percentiles to fractions

--- scripts/generate/district-descriptives.R
+++ scripts/generate/district-descriptives.R
@@ -11,24 +11,11 @@ rm(list = ls())
 # inputs 
 
 floods.path <- "intermediate/floods/floods.rds"
-survey.path <- "intermediate/floods/survey-sub-2012.rds"
-trips.path <- "intermediate/floods/floods-model.rds"
-
-sub.path <- "stores/floods/LAYER_SUBPREFEITURAS_2013/"
+sub.path <- "stores/floods/LAYER_SUBPREFEITURAS_2013"
 
 # read data 
 
 floods <- readRDS(floods.path)
-survey <- readRDS(survey.path)
-trips <- readRDS(trips.path)
-
-# merge districts from 2012 survey with crawled trips data
-
-survey <- as.data.table(survey)
-survey <- survey[,c("ID_ORDEM", "SUB")]
-
-trips <- as.data.table(trips)
-trips <- merge(trips, survey, by = "ID_ORDEM", all.x = TRUE)
 
 # generate number of floods per district
 
@@ -37,56 +24,53 @@ floods1 <- summarise(floods, count = n())
 
 # names of subprefectures
 
-sub <- readOGR(sub.path, "DEINFO_SUBPREFEITURAS_2013")
-
-# Assign CRS: SAD69 (cf. "deinfometadadossubprefeituras2013.csv")
-proj4string(sub) <- CRS("+proj=poly +lat_0=0 +lon_0=-54 +x_0=5000000 +y_0=10000000 +ellps=aust_SA +units=m +no_defs ")
+sub <- readOGR(dsn = sub.path, layer = "DEINFO_SUBPREFEITURAS_2013")
 
 # Transform SAD69 to WGS84
 sub <- spTransform(sub, CRS("+proj=longlat +ellps=WGS84"))
 
 # Extract data 
-sub <- as.data.frame(sub@data)
+sub.df <- as.data.frame(sub@data)
 
 # Subset to names of districts
 
-sub <- as.data.frame(sub$NOME)
-names(sub)[names(sub) == "sub$NOME"] <- "NOME"
+sub.df <- as.data.frame(sub$NOME)
+names(sub.df)[names(sub.df) == "sub$NOME"] <- "NOME"
 
 # Add abbreviation
 
-sub$SUB <- ifelse(sub$NOME == "ARICANDUVA", "AF",
-                     ifelse(sub$NOME == "BUTANTA", "BT",
-                            ifelse(sub$NOME == "CAMPO LIMPO", "CL",
-                                   ifelse(sub$NOME == "CAPELA DO SOCORRO", "CS",
-                                          ifelse(sub$NOME == "CASA VERDE-CACHOEIRINHA", "CV",
-                                                 ifelse(sub$NOME == "CIDADE ADEMAR", "AD",
-                                                        ifelse(sub$NOME == "CIDADE TIRADENTES", "CT",
-                                                               ifelse(sub$NOME == "ERMELINO MATARAZZO", "EM",
-                                                                      ifelse(sub$NOME == "FREGUESIA-BRASILANDIA", "FO",
-                                                                             ifelse(sub$NOME == "GUAIANAZES", "GU",
-                                                                                    ifelse(sub$NOME == "IPIRANGA", "IP",
-                                                                                           ifelse(sub$NOME == "ITAIM PAULISTA", "IT",
-                                                                                                  ifelse(sub$NOME == "ITAQUERA", "IQ",
-                                                                                                         ifelse(sub$NOME == "JABAQUARA", "JA",
-                                                                                                                ifelse(sub$NOME == "JACANA-TREMEMBE", "JT",
-                                                                                                                       ifelse(sub$NOME == "LAPA", "LA",
-                                                                                                                              ifelse(sub$NOME == "M'BOI MIRIM", "MB",
-                                                                                                                                     ifelse(sub$NOME == "MOOCA", "MO",
-                                                                                                                                            ifelse(sub$NOME == "PARELHEIROS", "PA",
+sub.df$SUB <- ifelse(sub.df$NOME == "ARICANDUVA", "AF",
+                     ifelse(sub.df$NOME == "BUTANTA", "BT",
+                            ifelse(sub.df$NOME == "CAMPO LIMPO", "CL",
+                                   ifelse(sub.df$NOME == "CAPELA DO SOCORRO", "CS",
+                                          ifelse(sub.df$NOME == "CASA VERDE-CACHOEIRINHA", "CV",
+                                                 ifelse(sub.df$NOME == "CIDADE ADEMAR", "AD",
+                                                        ifelse(sub.df$NOME == "CIDADE TIRADENTES", "CT",
+                                                               ifelse(sub.df$NOME == "ERMELINO MATARAZZO", "EM",
+                                                                      ifelse(sub.df$NOME == "FREGUESIA-BRASILANDIA", "FO",
+                                                                             ifelse(sub.df$NOME == "GUAIANAZES", "GU",
+                                                                                    ifelse(sub.df$NOME == "IPIRANGA", "IP",
+                                                                                           ifelse(sub.df$NOME == "ITAIM PAULISTA", "IT",
+                                                                                                  ifelse(sub.df$NOME == "ITAQUERA", "IQ",
+                                                                                                         ifelse(sub.df$NOME == "JABAQUARA", "JA",
+                                                                                                                ifelse(sub.df$NOME == "JACANA-TREMEMBE", "JT",
+                                                                                                                       ifelse(sub.df$NOME == "LAPA", "LA",
+                                                                                                                              ifelse(sub.df$NOME == "M'BOI MIRIM", "MB",
+                                                                                                                                     ifelse(sub.df$NOME == "MOOCA", "MO",
+                                                                                                                                            ifelse(sub.df$NOME == "PARELHEIROS", "PA",
                                                                                                                                                    ifelse(sub$NOME == "PENHA", "PE",
-                                                                                                                                                          ifelse(sub$NOME == "PERUS", "PR",
-                                                                                                                                                                 ifelse(sub$NOME == "PINHEIROS", "PI",
-                                                                                                                                                                        ifelse(sub$NOME == "PIRITUBA", "PJ",
-                                                                                                                                                                               ifelse(sub$NOME == "SANTANA-TUCURUVI", "ST",
-                                                                                                                                                                                      ifelse(sub$NOME == "SANTO AMARO", "SA",
-                                                                                                                                                                                             ifelse(sub$NOME == "SAO MATEUS", "SM",
-                                                                                                                                                                                                    ifelse(sub$NOME == "SAO MIGUEL", "MP",
-                                                                                                                                                                                                           ifelse(sub$NOME == "SAPOPEMBA", "SP",
-                                                                                                                                                                                                                  ifelse(sub$NOME == "SE", "SE",
-                                                                                                                                                                                                                         ifelse(sub$NOME == "VILA MARIA-VILA GUILHERME", "MG",
-                                                                                                                                                                                                                                ifelse(sub$NOME == "VILA MARIANA", "VM",
-                                                                                                                                                                                                                                       ifelse(sub$NOME == "VILA PRUDENTE", "VP", NA))))))))))))))))))))))))))))))))
+                                                                                                                                                          ifelse(sub.df$NOME == "PERUS", "PR",
+                                                                                                                                                                 ifelse(sub.df$NOME == "PINHEIROS", "PI",
+                                                                                                                                                                        ifelse(sub.df$NOME == "PIRITUBA", "PJ",
+                                                                                                                                                                               ifelse(sub.df$NOME == "SANTANA-TUCURUVI", "ST",
+                                                                                                                                                                                      ifelse(sub.df$NOME == "SANTO AMARO", "SA",
+                                                                                                                                                                                             ifelse(sub.df$NOME == "SAO MATEUS", "SM",
+                                                                                                                                                                                                    ifelse(sub.df$NOME == "SAO MIGUEL", "MP",
+                                                                                                                                                                                                           ifelse(sub.df$NOME == "SAPOPEMBA", "SP",
+                                                                                                                                                                                                                  ifelse(sub.df$NOME == "SE", "SE",
+                                                                                                                                                                                                                         ifelse(sub.df$NOME == "VILA MARIA-VILA GUILHERME", "MG",
+                                                                                                                                                                                                                                ifelse(sub.df$NOME == "VILA MARIANA", "VM",
+                                                                                                                                                                                                                                       ifelse(sub.df$NOME == "VILA PRUDENTE", "VP", NA))))))))))))))))))))))))))))))))
 
 
 
@@ -94,31 +78,46 @@ sub$SUB <- ifelse(sub$NOME == "ARICANDUVA", "AF",
 
 # Merge floods
 
-sub <- merge(sub, floods1, by = "SUB", all.x = TRUE)
+sub.df <- merge(sub.df, floods1, by = "SUB", all.x = TRUE)
 
 # replace NAs with 0s
 
-sub$count[is.na(sub$count)] <- 0
+sub.df$count[is.na(sub.df$count)] <- 0
 
+# Fraction of flood events per district
 
-
-# Define function for generating percentiles
-
-pct <- ecdf(sub$count)
-
-# Generate percentiles
-
-sub$pct <- pct(sub$count)
+sub.df$fraction <- sub.df$count / 740
 
 # Sort according to percentiles (descending order)
 
-sub <- sub[order(-sub$pct),]
+sub.df <- sub.df[order(-sub.df$fraction),]
 
 # Output
 
-sub1 <- sub[,c("NOME", "pct")]
+sub1 <- sub.df[,c("NOME", "fraction")]
 xtable(sub1)
 
+# Plot 
 
+plot.data <- ggplot2::fortify(sub, region = "NOME")
 
+plot.data$NOME <- factor(plot.data$id)
+plot.data$id <- NULL
 
+plot.data <- merge(plot.data, sub1, by = "NOME", all.x = TRUE)
+
+ggplot(plot.data, aes(x = long, y = lat, group = group, fill = fraction)) +
+    ggtitle("Fraction of Flood Events Per District") +
+    viridis::scale_fill_viridis(direction = -1, begin = 0.1, end = 0.5) +
+    geom_polygon() +
+    coord_equal() +
+    theme_void() +
+    theme(legend.position = "bottom",
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid = element_blank(),
+          axis.title = element_blank())
+
+ggsave(paste0(out.path, "floods-frac.png"), height = 5, width = 8, dpi = 300)
+       
+       
